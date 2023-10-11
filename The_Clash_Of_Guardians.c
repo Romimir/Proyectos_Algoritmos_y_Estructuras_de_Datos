50ce5ae99083c762245be687298d724ac3deae86
Romimir <romina.villagra.aliaga.2004@gmail.com>
2023-10-11 00:02:47 -0300

    añadir carta
    falta la verificacion de nombre y clase

--- The_Clash_Of_Guardians.c
+++ The_Clash_Of_Guardians.c
@@ -5,13 +5,57 @@
 //estructura para leer Cartas.txt
 typedef struct cartas
 {
-    char Nombre[50];
+    char Nombre[10];
     char Clase;
     int Vida;
     int Ataque;
     int Defensa;
-} Cartas;
 
+} Cartas_Juego;
+
+void Crear_Carta(Cartas_Juego * carta_creada)
+{
+    printf("***********************************************\n");
+    printf("* Bienvenido al apartado para crear una carta *\n");
+    printf("*     ¿Que nombre desea darle al guardian?     *\n");
+    scanf("%c", &carta_creada->Nombre);
+
+    printf("¿De que clase sera el guardian? \n");
+    printf("Recuerda que debes ingresar una de las siguientes");
+    printf("Mago, Vikingo, Nigromante, Bestia");
+    scanf("%c", &carta_creada->Clase);
+
+    printf("¿Cuanta vida posee el guardian? \n"); 
+    do
+    {
+        printf("La vida debe ser un valor entre 1 y 200\n");
+        scanf("%d", &carta_creada->Vida);
+        if (carta_creada->Vida <1 || carta_creada->Vida > 200)
+        {
+            printf("Por favor ingrese un valor para vida valido\n");
+        }
+        
+    } while (carta_creada->Vida < 1 || carta_creada->Vida > 200);
+
+    printf("¿Cuanto ataque tiene el guardian?");
+
+    do
+    {   
+        printf("El ataque debe ser un valor entre 1 y 200\n");
+
+        scanf("%d", &carta_creada->Ataque);
+
+    } while (carta_creada->Ataque <1 || carta_creada->Ataque >200);
+    
+    printf("¿Cuanta defensa tiene el guardian?");
+    do
+    {   
+        printf("La defensa debe ser un valor entre 1 y 200\n");
+
+        scanf("%d", &carta_creada->Defensa);
+
+    } while (carta_creada->Defensa <1 || carta_creada->Defensa >200);
+}
 int main()
 {
     int Opcion;
@@ -27,6 +71,7 @@ int main()
     printf("*             Para jugar ingrese 3            *\n");
     printf("*    Si desea salir del programa ingrese 4    *\n");
     printf("***********************************************\n");
+    _sleep(5);
     scanf("%d", &Opcion);
     } while (Opcion >= 1 && Opcion <= 4);
 
@@ -46,6 +91,7 @@ int main()
 
     case 4:
         printf("¡Gracias por jugar the clash of guardians!\n");
+        _sleep(5);
         exit(1);
         break;
     }
